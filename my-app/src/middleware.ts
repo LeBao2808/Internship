ac6480ad2b58177c7e4db6a5987d4cf30f6b3291
LeBao2808 <126840009+LeBao2808@users.noreply.github.com>
2025-06-12 17:16:13 +0700

    update file middleware

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -5,10 +5,35 @@ import { getToken } from 'next-auth/jwt'
 
 export async function middleware(req: NextRequest) {
     // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-    const hasSessionToken = req.cookies.has('next-auth.session-token') || req.cookies.has('accessToken') || req.cookies.has('refreshToken')
-    if (!hasSessionToken) {
-      return NextResponse.redirect(new URL('/authen/login', req.url))
+    // const hasSessionToken = req.cookies.has('next-auth.session-token') || req.cookies.has('accessToken') || req.cookies.has('refreshToken')
+    // if (!hasSessionToken) {
+    //   return NextResponse.redirect(new URL('/authen/login', req.url))
+    // }
+    const secret = process.env.NEXTAUTH_SECRET;
+    const token = await getToken({ req, secret });
+  
+    const isLoginPage = req.nextUrl.pathname.startsWith("/authen/login");
+    const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
+  
+    // Nếu là trang login và người dùng đã có token → redirect về /
+    if (isLoginPage && token) {
+      return NextResponse.redirect(new URL("/", req.url));
     }
+  
+    // Kiểm tra nếu là route /admin
+    if (isAdminRoute) {
+      // Nếu chưa đăng nhập → redirect về login
+      if (!token) {
+        return NextResponse.redirect(new URL("/authen/login", req.url));
+      }
+  
+      // Nếu đã đăng nhập nhưng role không phải admin → redirect về /
+      if (token.role !== "admin") {
+        return NextResponse.redirect(new URL("/UI/blog", req.url));
+      }
+    }
+  
+
     return NextResponse.next()
   }
    
