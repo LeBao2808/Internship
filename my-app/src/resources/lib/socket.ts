9c08e6a63482351d0cd22cab6f4ab1f12d694ba6
LeBao2808 <126840009+LeBao2808@users.noreply.github.com>
2025-06-12 16:27:17 +0700

    comment socket

--- my-app/src/resources/lib/socket.ts
+++ my-app/src/resources/lib/socket.ts
@@ -1,7 +1,7 @@
 // lib/socket.ts
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io("https://devblog-z3ir.onrender.com", {
   reconnection: true,
   reconnectionAttempts: Infinity,
 });
