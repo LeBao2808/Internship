b708688dace29799d187df103d321cd156160e43
LeBao2808 <126840009+LeBao2808@users.noreply.github.com>
2025-07-29 11:16:07 +0700

    adđ choice category

--- my-app/src/components/CategoryModal.tsx
+++ my-app/src/components/CategoryModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Category } from "@/utils/type";
-import "./css/CategoryModal.css";
+import "./css/categoryModal.css";
 import React, { useEffect, useMemo, useState } from "react";
 import { useSession } from "next-auth/react";
 
